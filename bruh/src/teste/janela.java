62f2c6c637b0a88fb53bd0821661cb2f4b0c4a62
zero <khalil.guimaraes@escola.pr.gov.br>
2025-11-10 08:57:27 -0300

    ljghkfkh
    ll

--- bruh/src/teste/janela.java
+++ bruh/src/teste/janela.java
@@ -5,24 +5,23 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
-public class Janela extends JFrame {// vai herdar a classe usada em java teste
-    public Janela() {//método contrutor
-        this.setBounds(0,0,1920,1080);//método importado de jframe
+public class Janela extends JFrame {
+    public Janela() {
+        this.setBounds(0,0,1920,1080);
 
-        CardLayout controleTela = new CardLayout();//PAINEL DE telas, permitindo a troca de telas, mostra por padrão a primeira tela
-        JPanel telas = new JPanel(controleTela);//objeto telas
-        LoginPanel loginPanel = new LoginPanel(telas, this); //chama o método, utiliza o atributo telas para mostrar as telas
-        PrincipalPanel principalPanel = new PrincipalPanel(telas, this); //chama o método, utiliza o atributo telas para mostrar as telas, atributo this para puxar o JFrame
+        CardLayout controleTela = new CardLayout();
+        JPanel telas = new JPanel(controleTela);
+        LoginPanel loginPanel = new LoginPanel(telas, this);
+        PrincipalPanel principalPanel = new PrincipalPanel(telas, this); 
 
-        telas.add(loginPanel,"Tela Login");//adicionar as telas
-        telas.add(principalPanel,"Tela Principal");//adicionar as telas, dar nome entre aspas
+        telas.add(loginPanel,"Tela Login");
+        telas.add(principalPanel,"Tela Principal");
+        this.add(telas);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setUndecorated(true);
+        this.setVisible(true);
 
-        this.add(telas);//para adicionar na tela, fazer testes com os paineis
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//fechar a janela
-        this.setExtendedState(JFrame.MAXIMIZED_BOTH);//mim=nimiza e maximiza a janela
-        this.setUndecorated(true);//tira o cabecalho padrão
-        this.setVisible(true); //método importado de jframe e sempre adicionar no final
-
-        // essa janela vai ser a principal e o conteudo vai trocar dentro da janela como se ela fosse a tv e as outras janelas os canais, sendo chamados de painel
+        
     }
 }
